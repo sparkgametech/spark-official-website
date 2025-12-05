d30c631cf47f6cdf8758cf8cfa9e3188883f1374
anonymous <anonymous@gmail.com>
2025-12-05 22:30:16 +0800

    [fix] 修復圖標

--- src/vue/content/sections/LicenseSection.vue
+++ src/vue/content/sections/LicenseSection.vue
@@ -23,11 +23,7 @@
 
             <ArticleParagraph title="MIT 授權"
                               fa-icon="pi pi-hammer"
-                              :description="[
-                                  `Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:`,
-                                  `The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.`,
-                                  `THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`
-                              ]"/>
+                              :description="mitLicenseText"/>
 
             <ArticleParagraph title="免責聲明"
                               fa-icon="pi pi-exclamation-triangle"
@@ -49,6 +45,12 @@ import Breadcrumbs from "/src/vue/components/widgets/Breadcrumbs.vue"
 const props = defineProps({
     id: String
 })
+
+const mitLicenseText = [
+    'Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:',
+    'The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.',
+    'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.'
+]
 </script>
 
 <style lang="scss" scoped>
